1244ec426269e851d91d7d7e77c6864d2efe9d11
allan <allan@conecturma.com.br>
2018-02-22 10:38:28 -0300

    colocando metodos na class DbTurma

--- src/model/redis.py
+++ src/model/redis.py
@@ -1,16 +1,19 @@
 from walrus import *
 
-db = Database(host='localhost', port=6379, db=0)
-
+db = Database(host = 'localhost', port = 6379, db = 0)
 
 class DbAluno(Model):
-    __database__ = db
-    id = TextField(primary_key=True)
-    nome = TextField()
-    serie = TextField()
+     __database__ = db
+     id = TextField(primary_key=True)
+     serie = TextField()
 
+class DbTurma(Model):
+     __database__ = db
+     id = AutoIncrementField(primary_key=True)
+     turma_nome = TextField(index = True)
 
-class Turma(Model):
-    __database__ = db
-    id = TextField(primary_key=True)
-turma_nome = TextField()
+     def create_turma(self,turma):
+          return self.create(turma_nome=turma)
+
+     def read_turma(self):
+          return self.query(order_by=self.id)
