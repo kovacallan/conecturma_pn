0f5c28cd12bbf1f0410f541934c92e40227f475c
allan <allan@conecturma.com.br>
2018-02-28 17:28:53 -0300

    corrigindo o read aluno

--- src/control/aluno_controller.py
+++ src/control/aluno_controller.py
@@ -38,7 +38,7 @@ def read_aluno():
     :return: o dicionario com a id , usuario_nome e senha_aluno para ser usado pela tpl
     """
     usuarios = facade.ReadAlunoFacade()
-    return dict(aluno_id=usuarios['id'], aluno_nome=usuarios['usuario_nome'], senha_aluno=usuarios['usuario_senha'])
+    return dict(aluno_id=usuarios['id'],aluno_matricula = usuarios['matricula'], aluno_nome=usuarios['usuario_nome'], senha_aluno=usuarios['usuario_senha'])
 
 
 ####-- Deletar aluno(usuario) --####
