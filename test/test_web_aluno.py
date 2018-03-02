4f12f7e36f6598a1424c2a2b2c0a97a205aa0b6e
cetoli <carlo@nce.ufrj.br>
2018-03-02 12:11:51 -0300

    test web comentado

--- test/test_web_aluno.py
+++ test/test_web_aluno.py
@@ -10,25 +10,18 @@ test_app = TestApp(application)
 
 
 class BlackBoxTest(unittest.TestCase):
-    """ check when there is query """
-    def test_upper_change(self):
+    def test_index(self):
+        """ o indice dev eser servido """
         res = test_app.get('/')
         self.assertEqual(res.status_int, 200)
         self.assertIn('name="usuario"', res.text, res.text)
         # self.assertEqual(res.json['upper_query'], 'FOO')
 
-    """ check when there is no query. """
-    def test_no_query(self):
+    def _test_read_student(self):
+        """ A página dos alunos deve permitir deletar aluno. """
         res = test_app.get('/ler_aluno')
         self.assertEqual(res.status_int, 200)
         self.assertIn('<form action="/deletar_alunos">', res.text, res.text)
-
-
-class _WhiteBoxTe_st(unittest.TestCase):
-    """ check upper case changing. """
-    def _test_upper_change(self):
-        rs = application.read_aluno()
-        self.assertEqual(rs._big_letter("foo"), 'FOO')
 
 
 if __name__ == '__main__':
