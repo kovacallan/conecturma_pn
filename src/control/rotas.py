726b2091f73974bedeee22d39f9637e86196a599
carlos <carlos@conecturma.com.br>
2018-06-08 14:36:59 -0300

    jogos setando caminhos

--- src/control/rotas.py
+++ src/control/rotas.py
@@ -1,77 +1,32 @@
 from bottle import route, template
 
-@route('/UV1/UV1MAPA/')
-def mapa_universo():
-    return template('jogo/UV1/UV1MAPA/index.html')
+# @route('/UV1/<path:path>')
+# def av1_mapa(path):
+#     return template('jogo/UV1/{}'.format(path))
+#
+# @route('/UV1/UV1AV1/UV1AV1MAPA/')
+# def av1_mapa2():
+#     print('oir')
+#     return template('jogo/UV1/UV1AV1/UV1AV1MAPA/index.html')
+# @route('/UV1/UV1AV1/UV1AV1MAPA/')
+#
+# @route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1MAPA/')
+# def av1_mapa3():
+#     print('aqui?1')
+#     return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1MAPA/index.html')
 
-@route('/UV1/UV1AV1/UV1AV1MAPA/')
-def mapa_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1MAPA/index.html')
+# @route('/UV1/UV1AV1/UV1AV1UD1/<path:path>')
+# def av1_mapa3(path):
+#     print('aqui?')
+#     return template('jogo/UV1/UV1AV1/UV1AV1UD1/{}'.format(path))
 
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1MAPA/')
-def mapa_unidade_1_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1MAPA/index.html')
+# @route('/UV1/UV1AV1/UV1AV1UD1/<path')
 
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1MAPA/')
-def mapa_unidade_1_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1MAPA/index.html')
+# @route('/GLOBAL/<path:path>')
+# def teste(path):
+#     return template('jogo/GLOBAL/{}'.format(path))
 
-"""----------------------------Semana 1 Aventura 1----------------------------------------"""
 
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA01/')
-def mapa_oa_1_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA01/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA02/')
-def mapa_oa_2_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA02/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA03/')
-def mapa_oa_3_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA03/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA04/')
-def mapa_oa_4_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA04/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA05/')
-def mapa_oa_5_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA05/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA06/')
-def mapa_oa_6_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA06/index.html')
-
-"""----------------------------Semana 2 Aventura 1----------------------------------------"""
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1MAPA/')
-def mapa_unidade_1_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1MAPA/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA01/')
-def mapa_oa_1_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA01/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA02/')
-def mapa_oa_2_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA02/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA03/')
-def mapa_oa_3_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA03/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA04/')
-def mapa_oa_4_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA04/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA05/')
-def mapa_oa_5_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA05/index.html')
-
-@route('/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA06/')
-def mapa_oa_6_aventura_1():
-    return template('jogo/UV1/UV1AV1/UV1AV1UD1/UV1AV1UD1OA06/index.html')
-
-@route('/GLOBAL/player/')
-def av1_mapa4():
-    return template('jogo/GLOBAL/player/index.html')
+# @route('/<path:path>/<filename>')
+# def teste_all(path):
+#     return template('jogo/{}'.format(path))
