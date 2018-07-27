07853170aa039e8285a28973cb2caf5c0cfd9c9d
kovacallan <36544596+kovacallan@users.noreply.github.com>
2018-07-27 13:52:46 -0300

    Corrigindo Problema de conflito

--- src/index.py
+++ src/index.py
@@ -1,27 +1,23 @@
-from bottle import default_app,run,template
-from control.static_controller import *
-from control.ambiente_de_aprendizagem_controller import *
-from control.ambiente_de_gestao_de_aprendizagem_controller import *
-from control.ambiente_administrativo_controller import *
+from bottle import default_app, run, template, route
+from control.static import *
 from control.filtro_usuario_controller import *
-from control.login_controller import *
-from paste import httpserver
+from control.static_game import *
+from control.route import *
+def main():
+    import bottle
+    import os
 
-import bottle
-import os
+    view_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'view')
+    bottle.TEMPLATE_PATH.insert(0, view_path)
 
-view_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'view')
-bottle.TEMPLATE_PATH.insert(0, view_path)
+    application = default_app()
 
-
-application = default_app()
-
+    run(host='0.0.0.0', port=80, server='gunicorn', workers=4)
 
 @route('/error403')
 def error403():
     return template('error403.tpl')
 
-if __name__ == '__main__':
-    run(server='paste')
-    httpserver.serve(application, host='0.0.0.0', port=80)
 
+if __name__ == '__main__':
+    main()
