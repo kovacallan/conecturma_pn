428407501f0d8c556cedd156d33fbc55dd6cd966
kovacallan <allan@conecturma.com.br>
2018-08-22 14:34:21 -0300

    colocando um print de senha no observador para poder usar emails que nao existem

--- src/control/gestao_aprendizagem_controller.py
+++ src/control/gestao_aprendizagem_controller.py
@@ -97,6 +97,8 @@ def password_generate():
     senha=random.sample(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'z', '!','@','#','$','%','&','*'], 8)
     senha.sort()
+
+    print(senha)
     return ''.join(senha)
 
 def send_email_confirmation(nome, email):
