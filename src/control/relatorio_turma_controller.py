45c152365ac683ead0dd9b2d25a2c109fa1cae5d
kovacallan <allan@conecturma.com.br>
2018-10-22 12:42:02 -0300

    Fazendo a classe que vai gerar o relatorio de turma.

--- src/control/relatorio_turma_controller.py
+++ src/control/relatorio_turma_controller.py
@@ -1,17 +1,13 @@
 from facade.facade_main import Facade
+from control.dicionarios import TIPO_ESTRUTURA
 class RelatorioTurma(object):
 
     def __init__(self):
         self.facade = Facade()
         self._alunos = []
         self._descritores = None
-        self._desempenho = None
+        self._media_alunos = None
         self._pontuacao = None
-        self._porcentagem = None
-        self._vezes_jogada = None
-        self._media = None
-        self._media_final = None
-        self._porcentagem_solo = []
 
     def get_alunos(self, vinculo_rede = None, vinculo_escola = None, vinculo_turma = None):
         if(vinculo_rede != None):
@@ -23,3 +19,44 @@ class RelatorioTurma(object):
                 self._alunos.append(i)
 
         return self._alunos
+
+
+    def get_descritores(self, serie):
+        self._descritores=self.facade.search_descritor_serie_facade(serie=serie)
+
+        return self._descritores
+
+    def get_media_alunos(self, turma):
+        alunos = self.get_alunos(vinculo_turma=turma)
+        for i in alunos:
+            desempenho = self.facade.search_desempenho_concluido_id_aluno_facade(id_aluno=i['id'])
+            pontuacao = self.convert_nivel_for_numeric(jogo_jogado=desempenho['jogo_jogado'])
+            for z in pontuacao:
+                print(z)
+
+
+    def convert_nivel_for_numeric(self, jogo_jogado):
+        niveis_pontuação = {
+            'dificil': 2,
+            'medio': 1,
+            'facil': 0
+        }
+
+        dicionario = []
+        pontuacao = []
+        for z in ['jogo_jogado']:
+            dict_dado_jogo = self.convertendo_str_in_dict(z)
+            if dict_dado_jogo['termino'] == True:
+                pontuacao.append(niveis_pontuação[dict_dado_jogo['nivel']])
+        dicionario.append(pontuacao)
+
+        self._pontuacao = dicionario
+
+        return self._pontuacao
+
+    def convertendo_str_in_dict(self, str):
+        from ast import literal_eval
+
+        python_dict = literal_eval(str)
+
+        return python_dict
--- src/route/relatorio_turma_route.py
+++ src/route/relatorio_turma_route.py
@@ -16,13 +16,8 @@ def relatorio_turma_view(no_repeat=False):
 def relatorio_aluno(no_repeat=False):
     observador = Observador(observador_logado=usuario_logado())
     relatorio = RelatorioTurma()
-    relarotio_aluno = Relatorio()
-
     turma = observador.get_turma(id_turma=request.params['turma'])
-    descritores = relarotio_aluno.get_descritores(serie=turma['serie'])
-    for i in relatorio.get_alunos(vinculo_turma=str(turma['id'])):
-        desempenho = relarotio_aluno.get_desempenho(descritores=descritores, aluno=i)
-
-    print(desempenho)
+    descritores = relatorio.get_descritores(serie=turma['serie'])
+    medias = relatorio.get_media_alunos(turma=turma['id'])
     #return template(path_template + 'relatorio_turma_detalhe',tipo=usuario_logado()['tipo'], aluno=relatorio.get_alunos(vinculo_turma=str(turma['id'])), oa=relarotio_aluno.descritores, porcentagem=relarotio_aluno.porcentagem,
     #          pontos=relarotio_aluno.porcentagem_solo,)
